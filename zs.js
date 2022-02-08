26b76b5767d8bb170dfa5f4a12776c6af36a41fe
ming38323 <96720169+ming38323@users.noreply.github.com>
2022-02-08 19:39:51 +0800

    Update zs.js

--- zs.js
+++ zs.js
@@ -2,3 +2,4 @@ hostname = *.zhuishushenqi.com
 https://sensorspop.zhuishushenqi.com/api/v2/sfo/user_popup_configs url script-request-header http://script.frp.aiuyo.com:8680/zssq.js
 https://goldcoin.zhuishushenqi.com/account/open-app url script-request-header http://script.frp.aiuyo.com:8680/zssq.js
 https://goldcoin.zhuishushenqi.com/thirdpartypromotion/v2/getAllPromotion url script-request-header http://script.frp.aiuyo.com:8680/zssq.js
+http://goldcoinnew.zhuishushenqi.com/redPacket/incentiveVideo url script-request-body http://script.frp.aiuyo.com:8680/zssq.js
