d7efc08872a261e2bc44cd294f25ff02522cf51e
ming38323 <96720169+ming38323@users.noreply.github.com>
2022-02-09 18:17:40 +0800

    Create dzl.js

---
+++ dzl.js
@@ -0,0 +1,4 @@
+hostname=api-sport.chenglie.tech
+
+api-sport.chenglie.tech/s.+ig url script-request-header https://raw.githubusercontent.com/bluesmallpig/scripts/main/dzl.js
+api-sport.chenglie.tech/wit.+/app.+ url script-request-body https://raw.githubusercontent.com/bluesmallpig/scripts/main/dzl.js
